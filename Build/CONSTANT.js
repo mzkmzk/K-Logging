33dbfe8ff0706bd2488fd73ec623a3b5479dbb10
maizhikun <maizhikun@xunlei.com>
2017-10-23 09:23:13 +0800

    feat: add .giignore

--- Build/CONSTANT.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-exports['default'] = {
-    URL: {
-        LOG_INSERT: 'http://k-inner-report.404mzk.com/v1/Creator_Log_Controller/query'
-    }
-};
--- Scripts/validate-commit-msg.js
+++
@@ -1,8 +0,0 @@
-var validateMessage = require('validate-commit-msg');
-
-var valid = validateMessage('chore(index): an example commit message');
-
-console.log(valid);
-
-
-console.log(1)
